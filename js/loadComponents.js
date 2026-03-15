9a272047da103fcaa45d47daec3b5d5ab658d3c1
Nikryss <giacobbenicholas88@gmail.com>
2026-03-15 11:34:27 +0000

    VERSIONE 0.2 ALPHA - Hotifix 0.1 -- Aggiunta Componente footer

--- js/loadComponents.js
+++ js/loadComponents.js
@@ -1,5 +1,11 @@
 fetch("components/header.html") /* Richiedo risorse al server, in questo caso del file del componente Header */
     .then(response => response.text()) /* Quando riesce a prendere la risorsa, restituisce un oggetto response */
-    .then(data => { /* Predni il contenuto del file HTML */
+    .then(data => { /* Prendi il contenuto del file HTML */
         document.getElementById("header-placeholder").innerHTML = data; /* Inserisci il contenuto ricavato dal file */
+    });
+
+fetch("components/footer.html") /* Richiedo risorse al server, in questo caso del file del componente Footer */
+    .then(response => response.text()) /* Quando riesce a prendere la risorsa, restituisce un oggetto response */
+    .then(data => { /* Prendi il contenuto del file HTML */
+        document.getElementById("footer-placeholer").innerHTML = data; /* Inserisci il contenuto ricavato dal file  */ 
     });
