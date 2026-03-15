35d31d1fe984d3a942982bf9eefae54e8b5869f1
Nikryss <giacobbenicholas88@gmail.com>
2026-03-15 11:21:52 +0000

    VERSIONE 0.1 APLHA - Hotfix 0.7 -- Aggiunta commenti sul codice fatto

--- js/loadComponents.js
+++ js/loadComponents.js
@@ -1,5 +1,5 @@
-fetch("components/header.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("header-placeholder").innerHTML = data;
+fetch("components/header.html") /* Richiedo risorse al server, in questo caso del file del componente Header */
+    .then(response => response.text()) /* Quando riesce a prendere la risorsa, restituisce un oggetto response */
+    .then(data => { /* Predni il contenuto del file HTML */
+        document.getElementById("header-placeholder").innerHTML = data; /* Inserisci il contenuto ricavato dal file */
     });
--- js/script.js
+++ js/script.js
@@ -1,51 +1,36 @@
-/* Nasconde tutte le sezioni inizialmente e mostra solamente quelle selezionate
-    Con questa funzione, permette una navigazione senza dove cambiare pagina
-*/
-
-function showSection(sectionId) {
-
-    let sections = document.querySelectorAll("main section");
-
-    sections.forEach(section => {
-        section.classList.remove("active")
-    });
-
-    document.getElementById(sectionId).classList.add("active");
-}
-
 /* ======================================
     Funzioni per aprire/chiudere la SideBar dell'header
 ========================================= */
 function toggleMenu() {
     
-    const sidebar = document.getElementById("sidebar");
-    const overlay = document.getElementById("overlay");
+    const sidebar = document.getElementById("sidebar"); /* Questa costante prende il valore dall'elemento con ID Sidebar */
+    const overlay = document.getElementById("overlay"); /* Questa costante prende il valore dall'elemento con ID Overlay */
 
-    sidebar.classList.toggle("open");
-    overlay.classList.toggle("active");
+    sidebar.classList.toggle("open"); /* Metti a questo elemento la classe CSS "Open" */
+    overlay.classList.toggle("active"); /* Metti a questo elemento la classe CSS "Active" */
 
-    document.body.classList.toggle("no-scroll");
+    document.body.classList.toggle("no-scroll"); /* Metti all'interno Body elemento la classe CSS "no-scroll" */
 
 }
 
 function closeMenu() {
 
-    const sidebar = document.getElementById("sidebar");
-    const overlay = document.getElementById("overlay");
+    const sidebar = document.getElementById("sidebar"); /* Questa costante prende il valore dall'elemento con ID Sidebar */
+    const overlay = document.getElementById("overlay"); /* Questa costante prende il valore dall'elemento con ID Overlay */
 
-    sidebar.classList.remove("open");
-    overlay.classList.remove("active");
+    sidebar.classList.remove("open"); /* Togli a questo elemento la classe CSS "Open" */
+    overlay.classList.remove("active"); /* Togli a questo elemento la classe CSS "Active" */
 
-    document.body.classList.remove("no-scroll");
+    document.body.classList.remove("no-scroll"); /* Togli all'interno Body elemento la classe CSS "no-scroll" */
 
 }
 
 /*================================================ 
     Funzione per chiudere la SideBar quando si seleziona una categoria
 ================================================== */
-document.addEventListener("click", function(e) {
+document.addEventListener("click", function(e) { /* Prendi qualsiasi azioni che succede sulla pagina Web */
     
-    if(e.target.classList.contains("menu-link")) {
-        closeMenu();
+    if(e.target.classList.contains("menu-link")) { /* Se l'azione è stata eseguita sopra a una categoria della Sidebar */
+        closeMenu(); /* Attiva questa funzione per chiudere la sidebar */
     }
 });
